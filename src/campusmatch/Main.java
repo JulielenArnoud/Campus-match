e233e59d981add3a89003df0be1ffdc4e72f1fcf
Julielen <julielenarnoudd@gmail.com>
2026-06-30 21:16:59 -0300

    refactor

--- src/campusmatch/Main.java
+++ src/campusmatch/Main.java
@@ -1,7 +1,16 @@
 package campusmatch;
 
+import campusmatch.dominio.Disciplina;
+import campusmatch.dominio.Grade;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        Disciplina disciplina = new Disciplina("CC100", "POO", 60, Set.of("Java"));
+        Grade grade = new Grade();
+
+        System.out.println(disciplina.getNome() + " carregada com sucesso.");
+        System.out.println("Alocações na grade inicial: " + grade.getAlocacoes().size());
 
     }
 }
--- src/campusmatch/dominio/Grade.java
+++ src/campusmatch/dominio/Grade.java
@@ -2,6 +2,7 @@ package campusmatch.dominio;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 /**
  * The type Grade.
@@ -13,7 +14,21 @@ public class Grade {
      * Instantiates a new Grade.
      */
     public Grade() {
+
         this.alocacoes = new ArrayList<>();
+    }
+
+    /**
+     * Adiciona uma nova alocacao na grade.
+     *
+     * @param alocacao the alocacao
+     */
+    public void adicionarAlocacao (Alocacao alocacao) {
+        if (alocacao == null) {
+            throw new IllegalArgumentException("A alocação não pode ser nula.");
+        }
+
+        this.alocacoes.add(alocacao);
     }
 
     /**
@@ -22,6 +37,6 @@ public class Grade {
      * @return the alocacoes
      */
     public List<Alocacao> getAlocacoes() {
-        return alocacoes;
+        return Collections.unmodifiableList(alocacoes);
     }
 }
